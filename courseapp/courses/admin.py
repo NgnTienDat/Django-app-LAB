a7e39ddf954ec864b4d9fb6afa662222ebb87eb5
Nguyen Tien Dat <tie.dat2004@gmail.com>
2024-10-22 22:03:42 +0700

    init

---
+++ courseapp/courses/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.utils.html import mark_safe
+from .models import Category, Course, Lesson, Tag
+
+# Register your models here.
+
+class LessonAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            'all': ('/static/css/main.css', )
+        }
+
+    list_display = ['id', 'subject', 'created_date', 'active', 'course']
+    search_fields = ['subject', 'created_date', 'course__subject']
+    list_filter = ['subject', 'course__subject']
+    readonly_fields = ['avatar']
+
+    def avatar(self, lesson):
+        return mark_safe(
+            "<img src='/static/{img_url}' alt='{alt}' width=120/>".format(img_url=lesson.image.name, alt=lesson.subject))
+
+
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
---
+++ courseapp/courses/models.py
@@ -0,0 +1,70 @@
+from email.iterators import body_line_iterator
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='uploads/%Y/%m')
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+
+class ItemBase(models.Model):
+    class Meta:
+        abstract = True
+    subject = models.CharField(max_length=255, null=False)
+    image = models.ImageField(upload_to='courses/%Y/%m', default=None)
+    created_date = models.DateTimeField(auto_now_add=True)  # luu thoi gian luc them lan dau
+    updated_date = models.DateTimeField(auto_now=True)  # luu thoi gian chinh sua
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class Course(ItemBase):
+    class Meta:
+        unique_together = ('subject', 'category')
+        ordering = ["-id"]
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+
+
+class Lesson(ItemBase):
+    class Meta:
+        unique_together = ('subject', 'course')
+    content = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    #test git push!!!!!!!!!!!!!!!!!!!!!
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
